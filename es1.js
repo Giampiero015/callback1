957fcbe8d2c9462220f35b95d6c0ae37ab231bf9
Giampiero015 <150815405+Giampiero015@users.noreply.github.com>
2023-11-27 11:41:47 +0100

    Add files via upload

---
+++ es1.js
@@ -0,0 +1,9 @@
+function doubleArray(arr, callback) {
+    return arr.map(numero=>(numero=callback(numero)));
+  }
+  function doubleValue(num) {
+    return num * 2;
+  }
+  const numbers = [1, 2, 3, 4, 5];
+  const doubledNumbers = doubleArray(numbers, doubleValue);
+  console.log(doubledNumbers); // Should output [2, 4, 6, 8, 10]
